2b2f0094ce281cd3b37a26abb0465511b615a273
Kanghee <kh63167574@gmail.com>
2026-07-29 21:33:24 +0900

    feat: user entity추가

---
+++ src/main/java/moly/backend/domain/user/domain/User.java
@@ -0,0 +1,35 @@
+package moly.backend.domain.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "tbl_user")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 128)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 30)
+    private String nickname;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+}
---
+++ src/main/java/moly/backend/domain/user/service/UserSignupService.java
@@ -0,0 +1,4 @@
+package moly.backend.domain.user.service;
+
+public class UserSignupService {
+}
